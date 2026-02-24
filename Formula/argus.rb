98784b2ff4ab36b7a2e130bb3ce296d22bb03f6b
lbarahona <lestermiller26@gmail.com>
2026-02-24 11:12:26 -0600

    Update argus to v0.3.0

--- Formula/argus.rb
+++ Formula/argus.rb
@@ -1,28 +1,30 @@
 class Argus < Formula
   desc "AI-powered observability CLI for SREs"
   homepage "https://github.com/lbarahona/argus"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
-    on_arm do
-      url "https://github.com/lbarahona/argus/releases/download/v0.2.0/argus_0.2.0_darwin_arm64.tar.gz"
-      sha256 "a9b1107f5b36d55db7cbb9773982b79d84402de0249bec170a085cd8c953a71e"
-    end
     on_intel do
-      url "https://github.com/lbarahona/argus/releases/download/v0.2.0/argus_0.2.0_darwin_amd64.tar.gz"
-      sha256 "bf549890cf87110c93696f734577213cedfe661c9ae8ca851a5b3aa5e1b895c2"
+      url "https://github.com/lbarahona/argus/releases/download/v0.3.0/argus_0.3.0_darwin_amd64.tar.gz"
+      sha256 "396a0527d6b954df63b5ead42471a341a8e8b07980330a6c35309bb089a3b626"
+    end
+
+    on_arm do
+      url "https://github.com/lbarahona/argus/releases/download/v0.3.0/argus_0.3.0_darwin_arm64.tar.gz"
+      sha256 "5184a216088fe7473185f7c3947316ab6c3eafd73c90a34eb54168252ef40505"
     end
   end
 
   on_linux do
-    on_arm do
-      url "https://github.com/lbarahona/argus/releases/download/v0.2.0/argus_0.2.0_linux_arm64.tar.gz"
-      sha256 "10d95b776bac743a96c9b7b22b07ce0c5392e15c07f9d33c0466282371a6575b"
-    end
     on_intel do
-      url "https://github.com/lbarahona/argus/releases/download/v0.2.0/argus_0.2.0_linux_amd64.tar.gz"
-      sha256 "954de6e8f44ec1daad4d333edf9276a63c80435f5f824faf9696181d1c327b7c"
+      url "https://github.com/lbarahona/argus/releases/download/v0.3.0/argus_0.3.0_linux_amd64.tar.gz"
+      sha256 "179e22890c222db1ed3be33101dd7171358447683269bf22913f6e8b2292f6fb"
+    end
+
+    on_arm do
+      url "https://github.com/lbarahona/argus/releases/download/v0.3.0/argus_0.3.0_linux_arm64.tar.gz"
+      sha256 "4459bd6d4799450daca0156ec9720940e12cfe74867d7ac80e8f7beaa3c2dc14"
     end
   end
 
@@ -31,6 +33,6 @@ class Argus < Formula
   end
 
   test do
-    assert_match "argus", shell_output("#{bin}/argus version 2>&1", 0)
+    system "#{bin}/argus", "version"
   end
 end
